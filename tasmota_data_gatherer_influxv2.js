fd327f2a2a526c7f6b75a3c8cdd219bd0e7cca13
Thewildbear <TheWildBear@users.noreply.github.com>
2021-09-23 16:51:16 +0200

    Adding a influxv2 version UNTESTED

---
+++ tasmota_data_gatherer_influxv2.js
@@ -0,0 +1,46 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+const Influx = require('influxdb-v2');
+var config = require('./config');
+const db = new Influx.InfluxDB(config.Influx);
+
+async function writeNewDataPoint(i) {
+	const res = await fetch('http://' + config.devices[i].ip + '/cm?cmnd=status%208');
+	const data = await res.json();
+	db.write(
+	{
+		org: config.orga,
+		bucket: config.bucket,
+		precision: 'ms'
+	},
+	[{
+			measurement: config.measurement,
+			tags: {host: config.devices[i].name},
+			fields:
+			{
+				power: data.StatusSNS.ENERGY.Power,
+				apperentpower: data.StatusSNS.ENERGY.ApparentPower,
+				reactivepower: data.StatusSNS.ENERGY.ReactivePower,
+				factor: data.StatusSNS.ENERGY.Factor,
+				today: data.StatusSNS.ENERGY.Today,
+				yesterday: data.StatusSNS.ENERGY.Yesterday,
+				voltage: data.StatusSNS.ENERGY.Voltage,
+				current: data.StatusSNS.ENERGY.Current
+			},
+	}].catch(error => {
+		console.error('Error saving data to InfluxDB!' + error)
+	});
+
+}
+
+function gather_and_save_data ()
+{
+	for(let counter=0; counter <= ((config.devices.length - 1) ); counter++)
+	{
+		writeNewDataPoint(counter);
+	}
+}
+
+setInterval(gather_and_save_data, 1000);
+
+
